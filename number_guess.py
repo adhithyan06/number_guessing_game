1179d299a17b08b53105b3477890adfecbddd5a3
Adhithyan VH <120294232+adhithyan06@users.noreply.github.com>
2023-07-25 21:08:49 +0530

    Add files via upload

---
+++ number_guess.py
@@ -0,0 +1,33 @@
+# Number guessing game using python
+
+import random
+import math
+
+lowernumber = int(input("Enter Lower limit Number: "))
+uppernumber = int(input("Enter Upper limit Number: "))
+
+x = random.randint(lowernumber, uppernumber)
+print(
+    "\n\tYou've only ",
+    round(math.log(uppernumber - lowernumber + 1, 2)),
+    " chances to guess the Number!\n",
+)
+
+count = 0
+
+while count < math.log(uppernumber - lowernumber + 1, 2):
+    count += 1
+
+    guess = int(input("Guess the number: "))
+
+    if x == guess:
+        print("Congratulations You guessed it in ", count, " tries Hooray!!")
+        break
+    elif x > guess:
+        print("You guessed a small number!")
+    elif x < guess:
+        print("You guessed a high number!")
+
+if count >= math.log(uppernumber - lowernumber + 1, 2):
+    print("\nThe number is %d" % x)
+    print("\tBetter luck next time!")
